f392d6eb95a74cd87cafd18006e4e5d84f39d11b
guoxh <guoxuehui@51jiabo.com>
2021-06-16 16:42:46 +0800

    host

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"/vue-next/"
+  base:"/vue-next/",
+  server: {
+    host: '192.168.9.136',
+    port: 8082
+  }
 })
