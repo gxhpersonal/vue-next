f002a99bdc40dca4933323e1c35b3ce9b4c44a48
guoxh <guoxuehui@51jiabo.com>
2021-07-15 15:54:06 +0800

    ssr

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
+// import { renderToString } from '@vue/server-renderer'
 import App from './App.vue'
 import router from "../router/index";
 import store from "../store/index";
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App).use(router).use(store).mount('#app')
